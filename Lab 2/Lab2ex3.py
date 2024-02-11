9da28984fcb0cf48655127652c019b792a6dda87
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-02-11 12:34:43 +0800

    Update Lab2ex3.py

--- Lab 2/Lab2ex3.py
+++ Lab 2/Lab2ex3.py
@@ -2,32 +2,20 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
-ret = False
-while not ret:
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to grab the first frame from the video source, Trying again...")
-    else:
-        print(f'Height: {frame.shape[0]}, Width: {frame.shape[1]}')
-
-frame = cap.read()
-
-height, width = frame.shape[:2]
-
 fourcc = cv.VideoWriter_fourcc(*'mp4v')
-out = cv.VideoWriter('output1.mp4', fourcc, 20.0, (width,height))
+out = cv.VideoWriter('output1.mp4', fourcc, 20.0, (640, 480))
 
 while (cap.isOpened()):
     ret, frame = cap.read()
-    if ret != True:
+
+    if not ret:
+        print("Can't recieve frame (stream end?). Exitting...")
         break
 
-
-    # write the frame
     out.write(frame)
 
     cv.imshow('frame',frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1)== ord('q'):
         break
 
 
