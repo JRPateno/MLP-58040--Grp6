2231049e9f715f55a7ec1b7bd78b9e8cfe459124
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-02-11 11:55:28 +0800

    Update supplementary.py

--- Lab 2/supplementary.py
+++ Lab 2/supplementary.py
@@ -1,80 +1,92 @@
 import cv2 as cv
-import random
-import pathlib
-from os import listdir
+from os import listdir, path
 from os.path import isfile, join
+from pathlib import Path
+import re
 
-onlyfiles = [f for f in listdir("C:\\Users\\STUDENT\\PycharmProjects\\pythonProject4") if isfile(join("C:\\Users\\STUDENT\\PycharmProjects\\pythonProject4", f))]
-outs = [f for f in onlyfiles if 'output' in f][-1][6]
+class Capture:
+    def __init__(self):
 
-while True:
-    number = outs
-    number = 1
-    print(number)
-    cap = cv.VideoCapture(0)
+        #   SET SAVE LOCATION HERE
+        self.path = r"C:\Users\schumarkie\PycharmProjects\supplementary"
 
-    ret = False
-    while not ret:
-        ret, frame = cap.read()
-        if not ret:
-            print("Failed to grab the first frame from the video source, Trying again...")
-        else:
-            print(f'Height: {frame.shape[0]}, Width: {frame.shape[1]}')
+        #   SET FILE NAME HERE
+        self.filename = 'output'
 
-    height, width = frame.shape[:2]
+        while True:
+            self.nameNumAddder()
+            self.videoCapture()
 
-    fourcc = cv.VideoWriter_fourcc(*'mp4v')
-    out = cv.VideoWriter(f'output{number}.mp4', fourcc, 60.0, (width,height))
+            choice = (input('\nReplay the recording[y/n]? ')).lower()
 
-    while (cap.isOpened()):
-        ret, frame = cap.read()
-        if ret != True:
-            break
+            if choice == 'y':
+                self.videoReplay()
 
-        # write the frame
-        out.write(frame)
+            choice = (input('Do you want to record again[y/n]? ')).lower()
 
-        cv.imshow('capture',frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+            if choice == 'n':
+                print("Goodbye...")
+                break
 
+            else:
+                continue
 
-    cap.release()
-    out.release()
-    cv.destroyWindow('capture')
+    def videoCapture(self):
+        cap = cv.VideoCapture(0)
 
-    user1 = input('replay? [y/n]: ')
+        height = int(cap.get(4))
+        width = int(cap.get(3))
 
-    if user1 == 'y':
-        cap2 = cv.VideoCapture(f'output{number}.mp4')
+        fourcc = cv.VideoWriter_fourcc(*'mp4v')
+        out = cv.VideoWriter(f'{self.newname}.mp4', fourcc, 60.0, (width, height))
 
-        while cap2.isOpened():
-            ret, frame = cap2.read()
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret != True:
+                break
+
+            # write the frame
+            out.write(frame)
+
+            cv.imshow('capture', frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        out.release()
+        cv.destroyWindow('capture')
+
+    def videoReplay(self):
+        cap = cv.VideoCapture(f'{self.newname}.mp4')
+
+        while cap.isOpened():
+            ret, frame = cap.read()
 
             if not ret:
-                print('Can\'t recieve frame (stream end?). Exiting...')
                 break
 
             cv.imshow('video', frame)
-            if cv.waitKey(1) == ord('q'):
+            if cv.waitKey(30) == ord('q'):
                 break
 
-        cap2.release()
+        cap.release()
         cv.destroyWindow('video')
+        return
 
-        user = input('record again? [y/n]: ')
+    def nameNumAddder(self):
+        if path.isfile(f'.\{self.filename}.mp4') == True:
 
-        if user == 'n':
-            break
+            try:
+                onlyFiles = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+                filteredFiles = [Path(f).stem for f in onlyFiles if self.filename in f and f.endswith('.mp4')]
+                num = [int(re.findall('\((.*\d)+\)', x)[0]) for x in filteredFiles if re.search(f'{self.filename}' + '.{1}\([0-9]+\)', x)]
+
+                self.newname = f'{self.filename} ({int(max(num))+1})'
+
+            except ValueError:
+                self.newname = f'{self.filename} (1)'
+
         else:
-            continue
+            self.newname = self.filename
 
-    elif user1 == 'n':
-        user = input('record again? [y/n]: ')
-
-        if user == 'n':
-            print("Exitting... Goodbye...")
-            break
-        else:
-            continue
-
+Capture()
