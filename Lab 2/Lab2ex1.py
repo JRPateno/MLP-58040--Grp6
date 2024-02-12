37c815ab73f78a8c629ddac5c81001c7bf5df229
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-02-12 17:39:31 +0800

    Update Lab2ex1.py

--- Lab 2/Lab2ex1.py
+++ Lab 2/Lab2ex1.py
@@ -2,12 +2,19 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
+#   Checks if cap variable is capturing
 if not cap.isOpened():
     print('Cannot open camera')
+
+    #   Built-in Python Function, terminates the script
     exit()
 
 while True:
+
+    #   ret variable checks if frame is retrieved and goes to the next one, returns false if camera closes.
+    #   frame variable is a decoded video frame in an array
     ret, frame = cap.read()
+    print(cap.read()[1])
     if not ret:
         print('Can\'t recieve frame (stream end?). Exiting...')
         break
@@ -18,3 +25,4 @@ while True:
 
 cap.release()
 cv.destroyAllWindows()
+
