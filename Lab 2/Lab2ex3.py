53ca32731cfdf95360a88c8e922d7f744ff7388d
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-02-12 18:41:12 +0800

    Update Lab2ex3.py

--- Lab 2/Lab2ex3.py
+++ Lab 2/Lab2ex3.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 
+#   VideoCapture - Class for video capturing from video files, image sequences or cameras.
 cap = cv.VideoCapture(0)
 
-
+# .fourcc() joins 4 characters into a fourcc code, a way of identifying data formats
 fourcc = cv.VideoWriter.fourcc(*'mp4v')
 
-
+#   VideoWriter - for writing video files or image sequences.
 out = cv.VideoWriter('output1.mp4', fourcc, 20.0, (640, 480))
 
 while (cap.isOpened()):
