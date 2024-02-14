8c3398086de9089652b2956ff1c8d72efc106d84
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-02-14 17:17:46 +0800

    Update Supplementary.py

--- Lab 3/Supplementary.py
+++ Lab 3/Supplementary.py
@@ -1,6 +1,16 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 
+def plot(row, column, index, img, title):
+    org = (200,200)
+    font = cv.FONT_HERSHEY_DUPLEX
+    fontScale = 3
+    color = (125, 246, 55)
+    thickness = 3
+
+    plt.subplot(row, column, index), plt.imshow(cv.putText(img, title, org, font, fontScale, color, thickness))
+    plt.xticks([]), plt.yticks([])
+
 img = cv.cvtColor(cv.imread('gosling.jpg'), cv.COLOR_BGR2RGB)
 
 blur = cv.blur(img, (5, 5))
@@ -11,20 +21,11 @@ mblur = cv.medianBlur(img, 5)
 
 bblur = cv.bilateralFilter(img, 9, 75, 75)
 
-plt.subplot(3, 3, 2), plt.imshow(img), plt.title('Original')
-plt.xticks([]), plt.yticks([])
-
-plt.subplot(3, 3, 4), plt.imshow(blur), plt.title('Blur')
-plt.xticks([]), plt.yticks([])
-
-plt.subplot(3, 3, 6), plt.imshow(gblur), plt.title('Gaussian Blur')
-plt.xticks([]), plt.yticks([])
-
-plt.subplot(3, 3, 7), plt.imshow(mblur), plt.title('Median Blur')
-plt.xticks([]), plt.yticks([])
-
-plt.subplot(3, 3, 9), plt.imshow(bblur), plt.title('Bilateral Filter')
-plt.xticks([]), plt.yticks([])
+plot(3, 3, 5, img, 'Original')
+plot(3, 3, 1, blur, 'Blurred')
+plot(3, 3, 3, gblur, 'Gaussian Blur')
+plot(3, 3, 7, mblur, 'Median Blur')
+plot(3, 3, 9, bblur, 'Bilateral Filter')
 
 plt.subplots_adjust(wspace=0, hspace=0.3)
 plt.show()
