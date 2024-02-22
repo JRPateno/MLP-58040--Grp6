f9437ea1e2a28c287fb13c904739ad5adf049561
MiguelSilleza <98940269+Silleza-Miguel@users.noreply.github.com>
2024-02-22 17:07:34 +0800

    Update lab4ex2.py

--- Lab 4/lab4ex2.py
+++ Lab 4/lab4ex2.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # img = cv.cvtColor(cv.imread('Liza-Soberano.jpg'), cv.COLOR_BGR2RGB)
 
 img = cv.imread('Liza-Soberano.jpg', 0)
+
 edges = cv.Canny(img, 100, 200)
 
 plt.figure(figsize=(20, 20))
